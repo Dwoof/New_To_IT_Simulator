d68cfa421dca97ebee48189fc4e8dc771467ad99
Derek Woughter <61754766+Dwoof@users.noreply.github.com>
2022-02-01 13:40:12 -0500

    Create New_To_IT_Simulator.py

---
+++ New_To_IT_Simulator.py
@@ -0,0 +1,6 @@
+print("Welcome to IT.")
+job = input("Do you want an entry level job? (enter 'yes')\n")
+if job.lower() == "yes":
+    print("Insufficient work experience")
+else:
+    print("No job for you")
